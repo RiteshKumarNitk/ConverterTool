68837e680978a4e72701b5efee3c24d7025f2851
Ritesh Kumar <rav332211@gmail.com>
2025-07-13 16:45:03 +0530

    ddd

--- src/features/AnyToJpeg/hooks/useFilterWithName.tsx
+++ src/features/AnyToJpeg/hooks/useFilterWithName.tsx
@@ -16,8 +16,8 @@ export const useFilterWithName = () => {
     setError(null);
     setIsConverting(true);
 
-    if (files.length > 1500) {
-      setError('You can upload up to 1500 files only.');
+    if (files.length > 2500) {
+      setError('You can upload up to 2500 files only.');
       setIsConverting(false);
       return;
     }
